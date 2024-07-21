cef065010656766ca2d898044bd51cde2656b7ca
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-07-20 20:48:33 -0400

    authentication & authorization using InMemoryUserDetailsManager

--- src/main/java/com/example/myrest/SecurityConfiguration.java
+++ src/main/java/com/example/myrest/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.example.myrest;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -9,44 +9,42 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-            return httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-                authorizationManagerRequestMatcherRegistry.requestMatchers("/home").permitAll();
-                authorizationManagerRequestMatcherRegistry.requestMatchers("/admin/**").hasRole("ADMIN");
-                authorizationManagerRequestMatcherRegistry.requestMatchers("/user/**").hasRole("USER");
-                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
-                    })
-                    .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                    .build();
-
+        return httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                    authorizationManagerRequestMatcherRegistry.requestMatchers("/home").permitAll();
+                    authorizationManagerRequestMatcherRegistry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    authorizationManagerRequestMatcherRegistry.requestMatchers("/user/**").hasRole("USER");
+                    authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+                })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
     }
+
+
+
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         UserDetails normalUser = User.builder()
-                .username("regular")
-                .password("$2a$12$fi5s2wFbNIqgf7f73QqyZONRn434GSpBtUKL9AltvSL7r/uJo.dj.")
+                .username("gc")
+               .password(bCryptPasswordEncoder().encode("1234"))
                 .roles("USER")
                 .build();
 
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password("$2a$12$Fl7wgD2Ckmp1m.U04clcT.dTDGZXqITEu52CsRqMLxqOnjkeJW2Ze")
-                .roles("ADMIN", "USER")
-                .build();
 
-        return new InMemoryUserDetailsManager(normalUser, adminUser);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new InMemoryUserDetailsManager(normalUser);
     }
 }
